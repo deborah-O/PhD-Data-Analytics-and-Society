f4452ea73667dd66b93f5052d9ad756c114400a2
Deborah <43342021+deborah-O@users.noreply.github.com>
2026-03-05 21:33:07 +0000

    Create run_model.py

---
+++ src/run_model.py
@@ -0,0 +1,27 @@
+from abm import Model, set_seed
+from plotting.style import apply_style
+
+
+def main():
+
+    apply_style()
+
+    set_seed(122)
+
+    agent_groups = {
+        "Group1": [0.2, 0.01, 1.0]
+    }
+
+    model = Model(
+        num_of_agents=1000,
+        num_iterations=1000,
+        agent_groups=agent_groups
+    )
+
+    model.run()
+
+    print(model.result.tail())
+
+
+if __name__ == "__main__":
+    main()
